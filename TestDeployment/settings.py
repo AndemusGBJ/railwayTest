fe29fbaa120c94db8208e6ac03b49c8da4af870c
AndemusGBJ <andemus017@outlook.fr>
2023-02-04 16:46:37 +0300

    Config SQLite

--- TestDeployment/settings.py
+++ TestDeployment/settings.py
@@ -78,16 +78,18 @@ WSGI_APPLICATION = 'TestDeployment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+## Connexion with postgreSql
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+# }
 
 
 
